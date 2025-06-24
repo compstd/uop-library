f7c0ffb5d980008730debfa4cd9231ee6080bb0c
compstd <104301608+compstd@users.noreply.github.com>
2025-06-24 16:12:03 +0500

    Update cloudinary.config.js

--- backend/config/cloudinary.config.js
+++ backend/config/cloudinary.config.js
@@ -1,11 +1,31 @@
+// cloudinary.config.js file
 require('dotenv').config();
 const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
-  cloud_name:  process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:     process.env.CLOUDINARY_API_KEY,
-  api_secret:  process.env.CLOUDINARY_API_SECRET,
-  secure:      true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
-module.exports = cloudinary;
+// Add the uploadToCloudinary function
+const uploadToCloudinary = (buffer, folder) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream(
+      {
+        resource_type: 'auto',
+        folder: folder,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    ).end(buffer);
+  });
+};
+
+module.exports = { cloudinary, uploadToCloudinary };
